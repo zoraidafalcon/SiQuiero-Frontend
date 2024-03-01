7568ba5270b4b8c982f54b90497d025da68ffb69
Virginiadm <virginia.dm44@gmail.com>
2024-03-01 11:46:06 +0000

    Login Done

--- SiQuiero-Frontend/src/pages/Login/Login.jsx
+++ SiQuiero-Frontend/src/pages/Login/Login.jsx
@@ -22,70 +22,84 @@ function Login() {
       <Grid className="row_login" container spacing={2}>
         {/* 1º fila */}
         <Grid className="colm colm_galeria" item xs={6} md={6}>
-          {/* foto 1/4 */}
+          {/* foto 1/3 */}
          
           <div className="caja_doble">
             <div className="foto">
               <div className="padre_imagen relative">
-                <div className="imagen"></div>
+                <div className="imagen galeria1"></div>
                 <div className="filtro_color absolute"></div>
               </div>
             </div>
-
-            {/* foto 2/4 */}
-            <div className="foto">
-              <div className="padre_imagen relative">
-                <div className="imagen"></div>
-                <div className="filtro_color absolute"></div>
-              </div>
-              </div>
           </div>
 
           <hr className="espacio"/>
 
           <div className="caja_doble">
-          {/* foto 3/4 */}
+          {/* foto 2/3 */}
           <div className="foto">
             <div className="padre_imagen relative">
-              <div className="imagen"></div>
+              <div className="imagen galeria2"></div>
               <div className="filtro_color absolute"></div>
             </div>
             </div>
         
-          {/* foto 4/4 */}
+          {/* foto 3/3 */}
           <div className="foto">
             <div className="padre_imagen relative">
-              <div className="imagen"></div>
+              <div className="imagen galeria3"></div>
               <div className="filtro_color absolute"></div>
             </div>
             </div>
           </div>
         </Grid>
 
-        {/* 1º columna */}
+        {/* imagen 4 */}
         <Grid className="colm" item xs={6} md={6}>
           <div className="padre_imagen relative">
-            <div className="imagen"></div>
+            <div className="imagen galeria4"></div>
             <div className="filtro_color absolute"></div>
           </div>
         </Grid>
 
-        {/* 2º fila */}
+        {/* imagen 5 */}
         <Grid className="colm" item xs={6} md={6}>
           <div className="padre_imagen relative">
-            <div className="imagen"></div>
+            <div className="imagen galeria5"></div>
             <div className="filtro_color absolute"></div>
           </div>
         </Grid>
 
         <Grid className="colm" item xs={6} md={6}>
-          <p>hola</p>
+        <div className="caja_doble alta">
+          {/* imagen 6 */}
+          <div className="foto finquierda">
+            <div className="padre_imagen relative">
+              <div className="imagen galeria6"></div>
+              <div className="filtro_color absolute"></div>
+            </div>
+            </div>
+        
+          {/* imagen 7 */}
+          <div className="foto fderecha">
+            <div className="padre_imagen relative">
+              <div className="imagen galeria7"></div>
+              <div className="filtro_color absolute"></div>
+            </div>
+            </div>
+          </div>
         </Grid>
       </Grid>
 
+      <div className="lateral centrado">
+      <div>
       <h1>¡Maravilloso!</h1>
       <p>Ahora sí, empieza la aventura</p>
       <LoginCard />
+      </div>
+      </div>
+
+     
     </div>
   );
 }
